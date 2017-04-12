a58f216f391dd0186d555820536e4adcdd6fc715
muralincodeit <murali.b@ncodeit.com>
2017-04-12 19:04:27 +0530

    i added testjava.java

---
+++ testjava.java
@@ -0,0 +1,15 @@
+class testjava
+{
+int a=1;
+int b=2;
+void show()
+{
+System.out.println("Value of a = "+a);
+System.out.println("Value of b = "+b);
+}
+public static void main(String args[])
+{
+testjava object1=new testjava();
+object1.show(); 
+}
+}
